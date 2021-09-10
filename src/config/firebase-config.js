1dc4132dd61d48a56559d7a4439d39fbcc9e1a31
Praveen Kumar <praveen@Praveens-MacBook-Pro.local>
2021-09-10 20:41:28 +0530

    Update config

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,9 +1,10 @@
 export default {
-    apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-    authDomain: "myaurapp.firebaseapp.com",
-    databaseURL: "https://myaurapp.firebaseio.com",
-    projectId: "myaurapp",
-    storageBucket: "myaurapp.appspot.com",
-    messagingSenderId: "419511997060",
-    appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
-}
+    apiKey: "AIzaSyDioclbEh-kYKZXZ5_9FdPqnMY6Mx3VxGc",
+    authDomain: "dsc-iiitu.firebaseapp.com",
+    databaseURL: "https://dsc-iiitu.firebaseio.com",
+    projectId: "dsc-iiitu",
+    storageBucket: "dsc-iiitu.appspot.com",
+    messagingSenderId: "602152914431",
+    appId: "1:602152914431:web:f061d61a92c8fa8624ebca"
+};
+  
